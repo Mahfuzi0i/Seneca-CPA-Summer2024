9cea55c8d8e0bf33134fbc35d91d631b4a3787ec
Mahfuzi0i <163027324+Mahfuzi0i@users.noreply.github.com>
2024-08-15 14:40:13 -0400

    Milestone 3

---
+++ OOP244ZFF/MS3/Utils.cpp
@@ -0,0 +1,41 @@
+/***********************************************************************
+// OOP244 Utils Module
+// File	Utils.cpp
+// Version
+// Date
+// Author
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+//
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+/* Citation and Sources...
+Final Project Milestone 3
+Module: Utils.cpp
+Filename: Utils.cpp
+Version 1.0
+Author	Abdullah Al Mahfuz
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2024/06/30  Preliminary release
+2024/06/30  Debugged DMA
+
+Name           : Abdullah Al Mahfuz
+Email          : aamahfuz@myseneca.ca
+Student ID     : 180377236
+Section        : ZFF
+Date           :
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+*/
+#include "Utils.h"
+namespace seneca {
+
+
+}
