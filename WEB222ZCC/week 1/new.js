ae20f9e95dbd38043385e99cace5e5874c12616c
Mahfuzi0i <163027324+Mahfuzi0i@users.noreply.github.com>
2024-08-15 15:03:15 -0400

    Add files via upload

---
+++ WEB222ZCC/week 1/new.js
@@ -0,0 +1,2 @@
+console.log("I guess this is my new terminal");
+console.log("this works a bit different from the C++");
---
+++ WEB222ZCC/week 1/welcome.js
@@ -0,0 +1,7 @@
+console.log("Welcome to java script!!");
+console.log("I found a very good professor!!\n");
+var a = 100;
+console.log(a);
+
+let b = `damn that is cool`;
+console.log(b);
