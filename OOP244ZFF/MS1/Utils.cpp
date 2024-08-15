95bdcc3c7fc99093a93bda2047eb8f5c7701d3d3
Mahfuzi0i <163027324+Mahfuzi0i@users.noreply.github.com>
2024-08-15 14:38:57 -0400

    Milestone 1

---
+++ OOP244ZFF/MS1/Utils.cpp
@@ -0,0 +1,41 @@
+/***********************************************************************
+// OOP244 Utils Module
+// File	Utils.cpp
+// Version
+// Date
+// Author
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+//
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+/* Citation and Sources...
+Final Project Milestone 1
+Module: Menu.cpp
+Filename: Menu.cpp
+Version 1.0
+Author	Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2024/06/30  Preliminary release
+2024/06/30  Debugged DMA
+
+Name           : Abdullah Al Mahfuz
+Email          : aamahfuz@myseneca.ca
+Student ID     : 180377236
+Section        : ZFF
+Date           : 11 July 2024 (Friday)
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+*/
+#include "Utils.h"
+namespace seneca {
+
+
+}
---
+++ OOP244ZFF/MS1/Utils.h
@@ -0,0 +1,43 @@
+/***********************************************************************
+// OOP244 Utils Module
+// File	Utils.h
+// Version 
+// Date	
+// Author	
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+// 
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+/* Citation and Sources...
+Final Project Milestone 1
+Module: Menu.cpp
+Filename: Menu.cpp
+Version 1.0
+Author	Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2024/06/30  Preliminary release
+2024/06/30  Debugged DMA
+
+Name           : Abdullah Al Mahfuz
+Email          : aamahfuz@myseneca.ca
+Student ID     : 180377236
+Section        : ZFF
+Date           : 11 July 2024 (Friday)
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+*/
+#ifndef SENECA_UTILS_H__
+#define SENECA_UTILS_H__
+
+namespace seneca {
+
+}
+#endif // SENECA_UTILS_H__
